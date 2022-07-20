9846744be1f7ce43f533af169592a238deb52d28
Nazmul Hossain <nazmuljuit@gmail.com>
2022-07-20 19:26:37 +0600

    update

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
+  <div class="wrapper">
       <HelloWorld msg="Exam is ready!" />
     </div>
   </header>
@@ -22,10 +20,7 @@ header {
   line-height: 1.5;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+
 
 @media (min-width: 1024px) {
   header {
@@ -34,9 +29,7 @@ header {
     padding-right: calc(var(--section-gap) / 2);
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+
 
   header .wrapper {
     display: flex;
